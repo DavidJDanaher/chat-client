5c874fe4ae1498011a673aafbebb7995e9adf6ff
David Danaher <ddanaher@solstice.com>
2018-01-18 16:13:42 -0600

    first commit: file creation

---
+++ scripts/chat.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function() {
+    console.log("Hello");
+    var question = '';
+    var test = $('.ava-header').text();
+
+    console.log(test);
+
+    $('.send-button').on('click', function(event) {
+        // console.log(event);
+
+        $('.chat-window:last-child').append();
+    });
+
+});
